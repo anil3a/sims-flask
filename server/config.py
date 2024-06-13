9f66da0ca2a64920603f68e756d683b198812b89
Anil Prajapati <anilprz3@gmail.com>
2024-06-13 18:35:36 +1000

    Add Flask, docker and docker build image with route to item

---
+++ server/config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///site.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+config_by_name = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
