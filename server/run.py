65e2d94cb89fed10a4bae586ac006635f41fe97a
Anil <anilprz3@gmail.com>
2024-06-21 01:22:29 +1000

    Add OPTIONS request handler

--- server/run.py
+++ server/run.py
@@ -1,5 +1,6 @@
 import os
 from app import create_app, db
+from flask import request, Response
 from flask.cli import FlaskGroup
 from flask_migrate import Migrate
 from flask_cors import CORS
@@ -12,6 +13,19 @@ migrate = Migrate(app, db)
 cors_allowed_origins = os.getenv('CORS_ALLOWED_ORIGINS', '*').split(',')
 CORS(app, resources={r"/*": {"origins": cors_allowed_origins}})
 
+@app.before_request
+def before_request():
+    if request.method == 'OPTIONS':
+        response = app.make_default_options_response()
+        headers = response.headers
+
+        origin = request.headers.get('Origin')
+        if origin in cors_allowed_origins:
+            headers['Access-Control-Allow-Origin'] = origin
+            headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PUT, DELETE'
+            headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
+
+        return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
